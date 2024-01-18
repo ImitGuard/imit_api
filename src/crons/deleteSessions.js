f4d5909f9c323e7195241f5b4aa72c9c9e4e6d0d
arellak <nimble0@protonmail.com>
2024-01-18 21:47:11 +0100

    fixed schema, removed hacky workaround

--- src/crons/deleteSessions.js
+++ src/crons/deleteSessions.js
@@ -3,13 +3,10 @@ import { prisma } from "../db.js";
 
 export const deleteSessions = async function(){
     try{
-        // ugly workaround since there is no native way to define the timezone in prisma - still no fix
-        const updatedDate = new Date(new Date().toLocaleString("sv-SE", { timeZone: "UTC" }));
-        Log.info("" + updatedDate);
         const deletedSessions = await prisma.sessions.deleteMany({
             where: {
                 expiresat: {
-                    lt: updatedDate,
+                    lt: new Date(),
                 },
             },
         });
